b13171a89b9a296bcd108ec10b46df449fc1de13
Saurav Rana <sauravrana646@gmail.com>
2022-09-04 12:32:44 +0530

    RDS added

--- ec2-rds/main.tf
+++ ec2-rds/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = "~> 1.2.8"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
-}
-
 module "ec2" {
   depends_on = [
     module.vpc
